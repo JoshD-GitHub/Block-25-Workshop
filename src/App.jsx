8c720751a266aa8e3c5569c26661ef62fa6a1518
Josh Duschik <josh.duschik@gmail.com>
2023-07-26 13:51:01 -0700

    contact list using api working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import './App.css';
+import ContactList from './components/ContactList';
 
 const App = () => {
   // const [count, setCount] = useState(0);
 
   return (
     <>
+      <ContactList />
     </>
   );
 };
