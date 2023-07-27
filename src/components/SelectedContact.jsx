065b2c35cc7b8cbf9af2babc57fa4a72b01f92f3
Josh Duschik <josh.duschik@gmail.com>
2023-07-26 22:08:21 -0700

    contact info working

--- src/components/SelectedContact.jsx
+++ src/components/SelectedContact.jsx
@@ -9,14 +9,33 @@ const SelectedContact = ({ selectedContactId, setSelectedContactId }) => {
           `https://jsonplaceholder.typicode.com/users/${selectedContactId}`
         );
         const data = await response.json();
-        console.log(data);
+        console.log('Contact',data);
         setContact(data);
       } catch (error) {
         console.error(error);
       }
     };
     fetchContacts();
-  }, []);
+  }, [selectedContactId]);
+
+  if (!contact) {
+    return <p>test</p>
+  };
+
+  return (
+    <>
+      <h1>{contact.name}</h1>
+      <p><strong>Username: </strong>{contact.username}</p>
+      <p><strong>Email: </strong>{contact.email}</p>
+      <p><strong>Phone: </strong>{contact.phone}</p>
+      <p><strong>Website: </strong>{contact.website}</p>
+      <h2>Address</h2>
+      <p><strong>City: </strong>{contact.address.city}</p>
+      <p><strong>Street: </strong>{contact.address.street}</p>
+      <p><strong>Zipcode: </strong>{contact.address.zipcode}</p>
+      
+    </>
+  );
 };
 
 export default SelectedContact;
